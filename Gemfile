e7aa3bdd7cf3196483a2e9c1de27b537e30ebe50
Stephen Huang <stephen.huang@reinteractive.net>
2019-02-24 22:29:00 +1100

    init app

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in zendesk_search.gemspec
+gemspec
---
+++ lib/zendesk_search.rb
@@ -0,0 +1,6 @@
+require "zendesk_search/version"
+
+module ZendeskSearch
+  class Error < StandardError; end
+  # Your code goes here...
+end
