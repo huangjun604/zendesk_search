e6718a29af12108e8824bd056212722d9bb44323
Stephen Huang <stephen.huang@reinteractive.net>
2019-02-25 23:48:57 +1100

    add basic function for search zendesk

--- lib/zendesk_search/cli.rb
+++ lib/zendesk_search/cli.rb
@@ -19,11 +19,37 @@ module ZendeskSearch
         when 'quit'
           exit(0)
         when '1'
-          nil
+          search_zendesk
         when '2'
           nil
         end
       end
+    end
+
+    private
+
+    def search_zendesk
+      case @cli.ask("Select 1) Users", ['1', 'quit'])
+      when 'quit'
+        exit(0)
+      when '1'
+        resource_class = Users
+      end
+
+      term = @cli.ask("Enter search term") { |q| q.in = resource_class.attribute_names }
+      value = @cli.ask("Enter search value")
+
+      @cli.say("Searching the <%= color('#{resource_class.to_s.downcase}', BOLD) %> for <%= color('#{term}', BOLD) %> with a value of <%= color('#{value}', BOLD) %>")
+      results = resource_class.load.find_by(term, value)
+
+      if results.empty?
+        @cli.say("No result found")
+      else
+        @cli.say("<%= color('#{results.size}', BOLD) %> results found")
+      end
+    end
+
+    def view_searchable_fields
     end
   end
 end
